346c1463f24b53cb49adcaa2633c4278a5498ba8
Mohamed Sameh <mohamedsameh2210@gmail.com>
2024-01-05 22:17:15 +0100

    Update data_types.hpp

--- include/data_types.hpp
+++ include/data_types.hpp
@@ -13,117 +13,126 @@ typedef unsigned int uint;
 using Vector4uc = Eigen::Matrix<unsigned char, 4, 1>;
 
 struct CameraParameters {
-    int image_width, image_height;
-    float focal_x, focal_y;
-    float principal_x, principal_y;
+	int image_width, image_height;
+	float focal_x, focal_y;
+	float principal_x, principal_y;
 
-    /**
-     * Returns camera parameters for a specified pyramid level; each level corresponds to a scaling of pow(.5, level)
-     * @param level The pyramid level to get the parameters for with 0 being the non-scaled version,
-     * higher levels correspond to smaller spatial size
-     * @return A CameraParameters structure containing the scaled values
-     */
-    CameraParameters level(const size_t level) const
-    {
-        if (level == 0) return *this;
+	/**
+	 * Returns camera parameters for a specified pyramid level; each level corresponds to a scaling of pow(.5, level)
+	 * @param level The pyramid level to get the parameters for with 0 being the non-scaled version,
+	 * higher levels correspond to smaller spatial size
+	 * @return A CameraParameters structure containing the scaled values
+	 */
+	CameraParameters level(const size_t level) const
+	{
+		if (level == 0) return *this;
 
-        const float scale_factor = powf(0.5f, static_cast<float>(level));
-        return CameraParameters { image_width >> level, image_height >> level,
-                                    focal_x * scale_factor, focal_y * scale_factor,
-                                    (principal_x + 0.5f) * scale_factor - 0.5f,
-                                    (principal_y + 0.5f) * scale_factor - 0.5f };
-    }
+		const float scale_factor = powf(0.5f, static_cast<float>(level));
+		return CameraParameters{ image_width >> level, image_height >> level,
+									focal_x * scale_factor, focal_y * scale_factor,
+									(principal_x + 0.5f) * scale_factor - 0.5f,
+									(principal_y + 0.5f) * scale_factor - 0.5f };
+	}
+
+	Eigen::Matrix3f getIntrinsicMatrix() const
+	{
+		Eigen::Matrix3f K;
+		K << focal_x, 0, principal_x,
+			0, focal_y, principal_y,
+			0, 0, 1;
+		return K;
+	}
 };
 
 struct FrameData {
-    std::vector<cv::Mat> depth_pyramid;
-    std::vector<cv::Mat> smoothed_depth_pyramid;
-    std::vector<cv::Mat> color_pyramid;
+	std::vector<cv::Mat> depth_pyramid;
+	std::vector<cv::Mat> smoothed_depth_pyramid;
+	std::vector<cv::Mat> color_pyramid;
 
-    std::vector<cv::Mat> vertex_pyramid;
-    std::vector<cv::Mat> normal_pyramid;
+	std::vector<cv::Mat> vertex_pyramid;
+	std::vector<cv::Mat> normal_pyramid;
 
-    explicit FrameData(const size_t pyramid_height) :
-            depth_pyramid(pyramid_height), smoothed_depth_pyramid(pyramid_height),
-            color_pyramid(pyramid_height), vertex_pyramid(pyramid_height), normal_pyramid(pyramid_height)
-    { }
+	explicit FrameData(const size_t pyramid_height) :
+		depth_pyramid(pyramid_height), smoothed_depth_pyramid(pyramid_height),
+		color_pyramid(pyramid_height), vertex_pyramid(pyramid_height), normal_pyramid(pyramid_height)
+	{ }
 
-    // No copying
-    FrameData(const FrameData&) = delete;
-    FrameData& operator=(const FrameData& other) = delete;
+	// No copying
+	FrameData(const FrameData&) = delete;
+	FrameData& operator=(const FrameData& other) = delete;
 
-    FrameData(FrameData&& data) noexcept :
-            depth_pyramid(std::move(data.depth_pyramid)),
-            smoothed_depth_pyramid(std::move(data.smoothed_depth_pyramid)),
-            color_pyramid(std::move(data.color_pyramid)),
-            vertex_pyramid(std::move(data.vertex_pyramid)),
-            normal_pyramid(std::move(data.normal_pyramid))
-    { }
+	FrameData(FrameData&& data) noexcept :
+		depth_pyramid(std::move(data.depth_pyramid)),
+		smoothed_depth_pyramid(std::move(data.smoothed_depth_pyramid)),
+		color_pyramid(std::move(data.color_pyramid)),
+		vertex_pyramid(std::move(data.vertex_pyramid)),
+		normal_pyramid(std::move(data.normal_pyramid))
+	{ }
 
-    FrameData& operator=(FrameData&& data) noexcept
-    {
-        depth_pyramid = std::move(data.depth_pyramid);
-        smoothed_depth_pyramid = std::move(data.smoothed_depth_pyramid);
-        color_pyramid = std::move(data.color_pyramid);
-        vertex_pyramid = std::move(data.vertex_pyramid);
-        normal_pyramid = std::move(data.normal_pyramid);
-        return *this;
-    }
+	FrameData& operator=(FrameData&& data) noexcept
+	{
+		depth_pyramid = std::move(data.depth_pyramid);
+		smoothed_depth_pyramid = std::move(data.smoothed_depth_pyramid);
+		color_pyramid = std::move(data.color_pyramid);
+		vertex_pyramid = std::move(data.vertex_pyramid);
+		normal_pyramid = std::move(data.normal_pyramid);
+		return *this;
+	}
 };
 
 struct ModelData {
-    std::vector<cv::Mat> vertex_pyramid;
-    std::vector<cv::Mat> normal_pyramid;
-    std::vector<cv::Mat> color_pyramid;
+	std::vector<cv::Mat> vertex_pyramid;
+	std::vector<cv::Mat> normal_pyramid;
+	std::vector<cv::Mat> color_pyramid;
 
-    ModelData(const size_t pyramid_height, const CameraParameters camera_parameters) :
-            vertex_pyramid(pyramid_height), normal_pyramid(pyramid_height),
-            color_pyramid(pyramid_height)
-    {
-        for (size_t level = 0; level < pyramid_height; ++level) {
-            vertex_pyramid[level] =
-                    cv::Mat(camera_parameters.level(level).image_height,
-                                                camera_parameters.level(level).image_width,
-                                                CV_32FC3);
-            normal_pyramid[level] =
-                    cv::Mat(camera_parameters.level(level).image_height,
-                                                camera_parameters.level(level).image_width,
-                                                CV_32FC3);
-            color_pyramid[level] =
-                    cv::Mat(camera_parameters.level(level).image_height,
-                                                camera_parameters.level(level).image_width,
-                                                CV_8UC3);
-            vertex_pyramid[level].setTo(0);
-            normal_pyramid[level].setTo(0);
-        }
-    }
+	ModelData(const size_t pyramid_height, const CameraParameters camera_parameters) :
+		vertex_pyramid(pyramid_height), normal_pyramid(pyramid_height),
+		color_pyramid(pyramid_height)
+	{
+		for (size_t level = 0; level < pyramid_height; ++level) {
+			vertex_pyramid[level] =
+				cv::Mat(camera_parameters.level(level).image_height,
+					camera_parameters.level(level).image_width,
+					CV_32FC3);
+			normal_pyramid[level] =
+				cv::Mat(camera_parameters.level(level).image_height,
+					camera_parameters.level(level).image_width,
+					CV_32FC3);
+			color_pyramid[level] =
+				cv::Mat(camera_parameters.level(level).image_height,
+					camera_parameters.level(level).image_width,
+					CV_8UC3);
+			vertex_pyramid[level].setTo(0);
+			normal_pyramid[level].setTo(0);
+		}
+	}
 
-    // No copying
-    ModelData(const ModelData&) = delete;
-    ModelData& operator=(const ModelData& data) = delete;
+	// No copying
+	ModelData(const ModelData&) = delete;
+	ModelData& operator=(const ModelData& data) = delete;
 
-    ModelData(ModelData&& data) noexcept :
-            vertex_pyramid(std::move(data.vertex_pyramid)),
-            normal_pyramid(std::move(data.normal_pyramid)),
-            color_pyramid(std::move(data.color_pyramid))
-    { }
+	ModelData(ModelData&& data) noexcept :
+		vertex_pyramid(std::move(data.vertex_pyramid)),
+		normal_pyramid(std::move(data.normal_pyramid)),
+		color_pyramid(std::move(data.color_pyramid))
+	{ }
 
-    ModelData& operator=(ModelData&& data) noexcept
-    {
-        vertex_pyramid = std::move(data.vertex_pyramid);
-        normal_pyramid = std::move(data.normal_pyramid);
-        color_pyramid = std::move(data.color_pyramid);
-        return *this;
-    }
+	ModelData& operator=(ModelData&& data) noexcept
+	{
+		vertex_pyramid = std::move(data.vertex_pyramid);
+		normal_pyramid = std::move(data.normal_pyramid);
+		color_pyramid = std::move(data.color_pyramid);
+		return *this;
+	}
 };
 
 struct Voxel {
-    double sdf; // signed distance function
-    double weight; // weight for averaging
-    Vector4uc color; // color
-    uint class_id;
+	double sdf; // signed distance function
+	double weight; // weight for averaging
+	Vector4uc color; // color
+	uint class_id;
 
-    Voxel() : sdf(0), weight(0), color(0, 0, 0, 0), class_id(0) { }
+	Voxel() : sdf(0), weight(0), color(0, 0, 0, 0), class_id(0) { }
 
 };
 
@@ -357,7 +366,31 @@ public:
 		return x * dy * dz + y * dz + z;
 	}
 
+	// Get volume as a opencv matrix
+	cv::Mat getVolume() {
+		int sizes[3] = { dx, dy, dz };
+		cv::Mat volume = cv::Mat(3, sizes, CV_32FC1, cv::Scalar(0));
+		for (int i = 0; i < dx; i++) {
+			for (int j = 0; j < dy; j++) {
+				for (int k = 0; k < dz; k++) {
+					volume.at<float>(i, j, k) = vol_access(i, j, k).sdf;
+				}
+			}
+		}
+	}
 
+	// Get weight as a opencv matrix
+	cv::Mat getWeight() {
+			int sizes[3] = { dx, dy, dz };
+			cv::Mat weight = cv::Mat(3, sizes, CV_32FC1, cv::Scalar(0));
+			for (int i = 0; i < dx; i++) {
+				for (int j = 0; j < dy; j++) {
+					for (int k = 0; k < dz; k++) {
+						weight.at<float>(i, j, k) = vol_access(i, j, k).weight;
+					}
+				}
+			}
+		}
 
 };
 
@@ -366,18 +399,18 @@ struct Frame {
 	const BYTE* color_map;
 	const float* depth_map;
 	const uint* class_map;
-	
+
 	std::vector<Eigen::Vector3f> verices;
 	std::vector<Eigen::Vector3f> normals;
 	Eigen::Matrix4f extrinsic;
 	Eigen::Matrix4f depth_extrinsics;
 
-Frame(const CameraParameters camera_parameters, const BYTE* color_map, const float* depth_map, const uint* class_map, Eigen::Matrix4f extrinsic, Eigen::Matrix4f depth_extrinsics) :
+	Frame(const CameraParameters camera_parameters, const BYTE* color_map, const float* depth_map, const uint* class_map, Eigen::Matrix4f extrinsic, Eigen::Matrix4f depth_extrinsics) :
 		camera_parameters(camera_parameters), color_map(color_map), depth_map(depth_map), class_map(class_map), extrinsic(extrinsic), depth_extrinsics(depth_extrinsics)
 	{
 	}
 
-Frame();
+	Frame();
 
 
 };
