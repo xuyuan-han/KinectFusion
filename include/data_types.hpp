eae5ee7b79b7deef4d8c6a99fa179b4096a3925e
Feng-Yang-github <feng.yang@tum.de>
2024-01-13 00:06:22 +0100

    finish surface prediction

---
+++ include/data_types.hpp
@@ -0,0 +1,451 @@
+#pragma once
+
+#include <Eigen/Eigen>
+#include <opencv2/opencv.hpp>
+
+#include <vector>
+#include <iostream>
+#include <cstring>
+#include <fstream>
+
+typedef unsigned char BYTE;
+typedef unsigned int uint;
+using Vector4uc = Eigen::Matrix<unsigned char, 4, 1>;
+
+struct CameraParameters {
+	int image_width, image_height;
+	float focal_x, focal_y;
+	float principal_x, principal_y;
+
+	/**
+	 * Returns camera parameters for a specified pyramid level; each level corresponds to a scaling of pow(.5, level)
+	 * @param level The pyramid level to get the parameters for with 0 being the non-scaled version,
+	 * higher levels correspond to smaller spatial size
+	 * @return A CameraParameters structure containing the scaled values
+	 */
+	CameraParameters level(const size_t level) const
+	{
+		if (level == 0) return *this;
+
+		const float scale_factor = powf(0.5f, static_cast<float>(level));
+		return CameraParameters{ image_width >> level, image_height >> level,
+									focal_x * scale_factor, focal_y * scale_factor,
+									(principal_x + 0.5f) * scale_factor - 0.5f,
+									(principal_y + 0.5f) * scale_factor - 0.5f };
+	}
+
+	Eigen::Matrix3f getIntrinsicMatrix() const
+	{
+		Eigen::Matrix3f K;
+		K << focal_x, 0, principal_x,
+			0, focal_y, principal_y,
+			0, 0, 1;
+		return K;
+	}
+};
+
+struct FrameData {
+	std::vector<cv::Mat> depth_pyramid;
+	std::vector<cv::Mat> smoothed_depth_pyramid;
+	std::vector<cv::Mat> color_pyramid;
+
+	std::vector<cv::Mat> vertex_pyramid;
+	std::vector<cv::Mat> normal_pyramid;
+
+	explicit FrameData(const size_t pyramid_height) :
+		depth_pyramid(pyramid_height), smoothed_depth_pyramid(pyramid_height),
+		color_pyramid(pyramid_height), vertex_pyramid(pyramid_height), normal_pyramid(pyramid_height)
+	{ }
+
+	// No copying
+	FrameData(const FrameData&) = delete;
+	FrameData& operator=(const FrameData& other) = delete;
+
+	FrameData(FrameData&& data) noexcept :
+		depth_pyramid(std::move(data.depth_pyramid)),
+		smoothed_depth_pyramid(std::move(data.smoothed_depth_pyramid)),
+		color_pyramid(std::move(data.color_pyramid)),
+		vertex_pyramid(std::move(data.vertex_pyramid)),
+		normal_pyramid(std::move(data.normal_pyramid))
+	{ }
+
+	FrameData& operator=(FrameData&& data) noexcept
+	{
+		depth_pyramid = std::move(data.depth_pyramid);
+		smoothed_depth_pyramid = std::move(data.smoothed_depth_pyramid);
+		color_pyramid = std::move(data.color_pyramid);
+		vertex_pyramid = std::move(data.vertex_pyramid);
+		normal_pyramid = std::move(data.normal_pyramid);
+		return *this;
+	}
+};
+
+struct ModelData {
+	std::vector<cv::Mat> vertex_pyramid;
+	std::vector<cv::Mat> normal_pyramid;
+	std::vector<cv::Mat> color_pyramid;
+
+	ModelData(const size_t pyramid_height, const CameraParameters camera_parameters) :
+		vertex_pyramid(pyramid_height), normal_pyramid(pyramid_height),
+		color_pyramid(pyramid_height)
+	{
+		for (size_t level = 0; level < pyramid_height; ++level) {
+			vertex_pyramid[level] =
+				cv::Mat(camera_parameters.level(level).image_height,
+					camera_parameters.level(level).image_width,
+					CV_32FC3);
+			normal_pyramid[level] =
+				cv::Mat(camera_parameters.level(level).image_height,
+					camera_parameters.level(level).image_width,
+					CV_32FC3);
+			color_pyramid[level] =
+				cv::Mat(camera_parameters.level(level).image_height,
+					camera_parameters.level(level).image_width,
+					CV_8UC3);
+			vertex_pyramid[level].setTo(0);
+			normal_pyramid[level].setTo(0);
+		}
+	}
+
+	// No copying
+	ModelData(const ModelData&) = delete;
+	ModelData& operator=(const ModelData& data) = delete;
+
+	ModelData(ModelData&& data) noexcept :
+		vertex_pyramid(std::move(data.vertex_pyramid)),
+		normal_pyramid(std::move(data.normal_pyramid)),
+		color_pyramid(std::move(data.color_pyramid))
+	{ }
+
+	ModelData& operator=(ModelData&& data) noexcept
+	{
+		vertex_pyramid = std::move(data.vertex_pyramid);
+		normal_pyramid = std::move(data.normal_pyramid);
+		color_pyramid = std::move(data.color_pyramid);
+		return *this;
+	}
+};
+
+struct Voxel {
+	double sdf; // signed distance function
+	double weight; // weight for averaging
+	Vector4uc color; // color
+	uint class_id;
+
+	Voxel() : sdf(0), weight(0), color(0, 0, 0, 0), class_id(0) { }
+
+};
+
+class Volume
+{
+
+private:
+	//! Lower left and Upper right corner.
+	Eigen::Vector3d min, max;
+
+	//! max-min
+	Eigen::Vector3d diag;
+
+	double ddx, ddy, ddz;
+	double dddx, dddy, dddz;
+
+	//! Number of cells in x, y and z-direction.
+	uint dx, dy, dz;
+
+	Voxel* vol;
+
+	double maxValue, minValue;
+
+	uint m_dim;
+
+	//! x,y,z access to vol*
+	inline Voxel vol_access(int x, int y, int z) const
+	{
+		return vol[getPosFromTuple(x, y, z)];
+	}
+public:
+	//! Initializes the volume.
+	Volume(Eigen::Vector3d min_, Eigen::Vector3d max_, uint dx_, uint dy_, uint dz_, uint dim)
+	{
+		min = min_;
+		max = max_;
+		diag = max - min;
+		dx = dx_;
+		dy = dy_;
+		dz = dz_;
+		m_dim = dim;
+		vol = NULL;
+
+		vol = new Voxel[dx * dy * dz];
+
+		compute_ddx_dddx();
+	}
+
+	~Volume()
+	{
+		delete[] vol;
+	};
+
+
+	//! Computes spacing in x,y,z-directions.
+	void compute_ddx_dddx()
+	{
+		ddx = 1.0f / (dx - 1); //This is how much the x value in voxel space changes per voxel assuming the volume is normalized to 1
+		ddy = 1.0f / (dy - 1);
+		ddz = 1.0f / (dz - 1);
+
+		dddx = (max[0] - min[0]) / (dx - 1); //This is how much the x value in real space changes per voxel
+		dddy = (max[1] - min[1]) / (dy - 1);
+		dddz = (max[2] - min[2]) / (dz - 1);
+
+		if (dz == 1)
+		{
+			ddz = 0;
+			dddz = 0;
+		}
+
+		diag = max - min;
+	}
+
+	//! Zeros out the memory
+	void Volume::zeroOutMemory()
+	{
+		for (uint i1 = 0; i1 < dx * dy * dz; i1++)
+			vol[i1].sdf = double(0.0);
+	}
+
+	//! Returns the Data.
+	Voxel* Volume::getData()
+	{
+		return vol;
+	};
+
+	//! Sets all entries in the volume to '0'
+	void clean()
+	{
+		for (uint i1 = 0; i1 < dx * dy * dz; i1++) vol[i1].sdf = double(0.0);
+	}
+
+	//! Sets minimum extension
+	void SetMin(Eigen::Vector3d min_)
+	{
+		min = min_;
+		diag = max - min;
+	}
+
+	//! Sets maximum extension
+	void SetMax(Eigen::Vector3d max_)
+	{
+		max = max_;
+		diag = max - min;
+	}
+	//! finds the minimum and maximum values in the volume
+	inline void computeMinMaxValues(double& minVal, double& maxVal) const
+	{
+		minVal = std::numeric_limits<double>::max();
+		maxVal = -minVal;
+		for (uint i1 = 0; i1 < dx * dy * dz; i1++)
+		{
+			double val = vol[i1].sdf;
+
+			if (minVal > val) minVal = val;
+			if (maxVal < val) maxVal = val;
+		}
+	}
+
+	//! Set the voxel at i.
+	inline void set(uint i, Voxel vox)
+	{
+		double val = vox.sdf;
+		if (val > maxValue)
+			maxValue = val;
+
+		if (val < minValue)
+			minValue = val;
+
+		vol[i] = vox;
+	};
+	//! Set the value at i.
+	inline void set(uint i, double val)
+	{
+		if (val > maxValue)
+			maxValue = val;
+
+		if (val < minValue)
+			minValue = val;
+
+		vol[i].sdf = val;
+	};
+	//! Set the voxel at (x_, y_, z_).
+	inline void set(uint x_, uint y_, uint z_, Voxel vox)
+	{
+		vol[getPosFromTuple(x_, y_, z_)] = vox;
+	};
+	//! Set the value at (x_, y_, z_).
+	inline void set(uint x_, uint y_, uint z_, double val)
+	{
+		vol[getPosFromTuple(x_, y_, z_)].sdf = val;
+	};
+
+	//!Get the voxel at i.
+	inline Voxel getVoxel(uint i) const
+	{
+		return vol[i];
+	};
+
+	//! Get the value at i.
+	inline double getValue(uint i) const
+	{
+		return vol[i].sdf;
+	};
+	//! Get the voxel at (x_, y_, z_).
+	inline Voxel getVoxel(uint x_, uint y_, uint z_) const
+	{
+		return vol[getPosFromTuple(x_, y_, z_)];
+	};
+
+	//! Get the value at (x_, y_, z_).
+	inline double getValue(uint x_, uint y_, uint z_) const
+	{
+		return vol[getPosFromTuple(x_, y_, z_)].sdf;
+	};
+
+	//! Get the value at (pos.x, pos.y, pos.z).
+	inline double getValue(const Eigen::Vector3i& pos_) const
+	{
+		return(getValue(pos_[0], pos_[1], pos_[2]));
+	}
+
+	//! Returns the cartesian x-coordinates of node (i,..).
+	inline double posX(int i) const
+	{
+		return min[0] + diag[0] * (double(i) * ddx);
+	}
+
+	//! Returns the cartesian y-coordinates of node (..,i,..).
+	inline double posY(int i) const
+	{
+		return min[1] + diag[1] * (double(i) * ddy);
+	}
+
+	//! Returns the cartesian z-coordinates of node (..,i).
+	inline double posZ(int i) const
+	{
+		return min[2] + diag[2] * (double(i) * ddz);
+	}
+
+	//! Returns the cartesian coordinates of node (i,j,k).
+	inline Eigen::Vector3d pos(int i, int j, int k) const
+	{
+		Eigen::Vector3d coord(0, 0, 0);
+
+		coord[0] = min[0] + (max[0] - min[0]) * (double(i) * ddx);
+		coord[1] = min[1] + (max[1] - min[1]) * (double(j) * ddy);
+		coord[2] = min[2] + (max[2] - min[2]) * (double(k) * ddz);
+
+		return coord;
+	}
+
+
+	//! Returns number of cells in x-dir.
+	inline uint getDimX() const { return dx; }
+
+	//! Returns number of cells in y-dir.
+	inline uint getDimY() const { return dy; }
+
+	//! Returns number of cells in z-dir.
+	inline uint getDimZ() const { return dz; }
+
+	inline Eigen::Vector3d getMin() { return min; }
+	inline Eigen::Vector3d getMax() { return max; }
+
+
+	//! Get the index of the cell using the tuple (x,y,z) in the flattened vector.
+	inline uint getPosFromTuple(int x, int y, int z) const
+	{
+		return x * dy * dz + y * dz + z;
+	}
+
+	// Get volume as a opencv matrix
+	cv::Mat getVolume() {
+		int sizes[3] = { dx, dy, dz };
+		cv::Mat volume = cv::Mat(3, sizes, CV_32FC1, cv::Scalar(0));
+		for (int i = 0; i < dx; i++) {
+			for (int j = 0; j < dy; j++) {
+				for (int k = 0; k < dz; k++) {
+					volume.at<float>(i, j, k) = vol_access(i, j, k).sdf;
+				}
+			}
+		}
+	}
+
+	// Get weight as a opencv matrix
+	cv::Mat getWeight() {
+			int sizes[3] = { dx, dy, dz };
+			cv::Mat weight = cv::Mat(3, sizes, CV_32FC1, cv::Scalar(0));
+			for (int i = 0; i < dx; i++) {
+				for (int j = 0; j < dy; j++) {
+					for (int k = 0; k < dz; k++) {
+						weight.at<float>(i, j, k) = vol_access(i, j, k).weight;
+					}
+				}
+			}
+		}
+
+};
+
+struct Frame {
+	const CameraParameters camera_parameters;
+	const BYTE* color_map;
+	const float* depth_map;
+	const uint* class_map;
+
+	std::vector<Eigen::Vector3f> verices;
+	std::vector<Eigen::Vector3f> normals;
+	Eigen::Matrix4f extrinsic;
+	Eigen::Matrix4f depth_extrinsics;
+
+	Frame(const CameraParameters camera_parameters, const BYTE* color_map, const float* depth_map, const uint* class_map, Eigen::Matrix4f extrinsic, Eigen::Matrix4f depth_extrinsics) :
+		camera_parameters(camera_parameters), color_map(color_map), depth_map(depth_map), class_map(class_map), extrinsic(extrinsic), depth_extrinsics(depth_extrinsics)
+	{
+	}
+
+	Frame();
+
+
+};
+
+/*
+    *
+    * \brief Contains the internal volume representation
+    *
+    * This internal representation contains two volumes:
+    * (1) TSDF volume: The global volume used for depth frame fusion and
+    * (2) Color volume: Simple color averaging for colorized vertex output
+    *
+    * It also contains two important parameters:
+    * (1) Volume size: The x, y and z dimensions of the volume (in mm)
+    * (2) Voxel scale: The scale of a single voxel (in mm)
+    *
+    */
+// -
+struct VolumeData {
+    // OpenCV 提供的在GPU上的图像数据类型
+    cv::Mat tsdf_volume; //short2
+    cv::Mat color_volume; //uchar4
+    Eigen::Vector3i volume_size;
+    float voxel_scale;
+
+    // 构造函数
+    VolumeData(const Eigen::Vector3i _volume_size, const float _voxel_scale) :
+            // 注意 TSDF 是2通道的, 意味着其中一个通道存储TSDF函数值, 另外一个通道存储其权重
+            // 这个第一维的尺寸有些奇特啊
+            tsdf_volume(cv::Mat(_volume_size[1] * _volume_size[2], _volume_size[0], CV_16SC2)),
+            color_volume(cv::Mat(_volume_size[1] * _volume_size[2], _volume_size[0], CV_8UC3)),
+            volume_size(_volume_size), voxel_scale(_voxel_scale)
+    {
+        // 全部清空
+        tsdf_volume.setTo(0);
+        color_volume.setTo(0);
+    }
+};
---
+++ src/surface_prediction.cpp
@@ -0,0 +1,305 @@
+#include "surface_prediction.hpp"
+
+#define DIVSHORTMAX 0.0000305185f 
+#define SHORTMAX    32767               // SHRT_MAX;
+#define MAX_WEIGHT  128                // max weight
+
+// 三线型插值
+float interpolate_trilinearly(
+    Eigen::Matrix<float, 3, 1, Eigen::DontAlign>& point,
+    cv::Mat& volume,  // TSDF Volume
+    const Eigen::Vector3f& volume_size,
+    const float voxel_scale)
+{
+    //cooridnate in grid and in int
+    Eigen::Matrix<int, 3, 1, Eigen::DontAlign> point_in_grid = point.cast<int>();
+
+    // voxel center
+    const float vx = (static_cast<float>(point_in_grid.x()) + 0.5f);
+    const float vy = (static_cast<float>(point_in_grid.y()) + 0.5f);
+    const float vz = (static_cast<float>(point_in_grid.z()) + 0.5f);
+       
+    point_in_grid.x() = (point.x() < vx) ? (point_in_grid.x() - 1) : point_in_grid.x();
+    point_in_grid.y() = (point.y() < vy) ? (point_in_grid.y() - 1) : point_in_grid.y();
+    point_in_grid.z() = (point.z() < vz) ? (point_in_grid.z() - 1) : point_in_grid.z();
+
+    // +0.5f 的原因是, point_in_grid 处体素存储的TSDF值是体素的中心点的TSDF值
+    // 三线型插值, ref: https://en.wikipedia.org/wiki/Trilinear_interpolation
+    // 计算精确的(浮点型)的点坐标和整型化之后的点坐标的差
+    const float a = (point.x() - (static_cast<float>(point_in_grid.x()) + 0.5f));
+    const float b = (point.y() - (static_cast<float>(point_in_grid.y()) + 0.5f));
+    const float c = (point.z() - (static_cast<float>(point_in_grid.z()) + 0.5f));
+
+    return 
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z()) * volume_size[1] + point_in_grid.y()),(point_in_grid.x()))[0]) * DIVSHORTMAX 
+            // volume[ x ][ y ][ z ], C000
+            * (1 - a) * (1 - b) * (1 - c) +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z() + 1) * volume_size[1] + point_in_grid.y()),(point_in_grid.x()))[0]) * DIVSHORTMAX 
+            // volume[ x ][ y ][z+1], C001
+            * (1 - a) * (1 - b) * c +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z()) * volume_size[1] + point_in_grid.y() + 1),(point_in_grid.x()))[0]) * DIVSHORTMAX 
+            // volume[ x ][y+1][ z ], C010
+            * (1 - a) * b * (1 - c) +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z() + 1) * volume_size[1] + point_in_grid.y() + 1),(point_in_grid.x()))[0]) * DIVSHORTMAX 
+            // volume[ x ][y+1][z+1], C011
+            * (1 - a) * b * c +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z()) * volume_size[1] + point_in_grid.y()),(point_in_grid.x() + 1))[0]) * DIVSHORTMAX 
+            // volume[x+1][ y ][ z ], C100
+            * a * (1 - b) * (1 - c) +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z() + 1) * volume_size[1] + point_in_grid.y()),(point_in_grid.x() + 1))[0]) * DIVSHORTMAX 
+            // volume[x+1][ y ][z+1], C101
+            * a * (1 - b) * c +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z()) * volume_size[1] + point_in_grid.y() + 1),(point_in_grid.x() + 1))[0]) * DIVSHORTMAX 
+            // volume[x+1][y+1][ z ], C110
+            * a * b * (1 - c) +
+        static_cast<float>(volume.at<cv::Vec<short, 2>>(static_cast<int>((point_in_grid.z() + 1) * volume_size[1] + point_in_grid.y() + 1),(point_in_grid.x() + 1))[0]) * DIVSHORTMAX 
+            // volume[x+1][y+1][z+1], C111
+            * a * b * c;
+}
+
+float get_min_time(
+    const Eigen::Vector3f& volume_max,
+    const Eigen::Matrix<float, 3, 1, Eigen::DontAlign>& origin,
+    const Eigen::Matrix<float, 3, 1, Eigen::DontAlign>& direction)
+{
+    float txmin = ((direction.x() > 0 ? 0.f : volume_max[0]) - origin.x()) / direction.x();
+    float tymin = ((direction.y() > 0 ? 0.f : volume_max[1]) - origin.y()) / direction.y();
+    float tzmin = ((direction.z() > 0 ? 0.f : volume_max[2]) - origin.z()) / direction.z();
+
+    return fmax(fmax(txmin, tymin), tzmin);
+}
+
+float get_max_time(
+    const Eigen::Vector3f& volume_max,
+    const Eigen::Matrix<float, 3, 1, Eigen::DontAlign>& origin,
+    const Eigen::Matrix<float, 3, 1, Eigen::DontAlign>& direction
+)
+{
+    float txmax = ((direction.x() > 0 ? volume_max[0] : 0.f) - origin.x()) / direction.x();
+    float tymax = ((direction.y() > 0 ? volume_max[1] : 0.f) - origin.y()) / direction.y();
+    float tzmax = ((direction.z() > 0 ? volume_max[2] : 0.f) - origin.z()) / direction.z();
+
+    return fmin(fmin(txmax, tymax), tzmax);
+}
+
+void raycast_tsdf_kernel(
+    cv::Mat& tsdf_volume,                       // global TSDF Volume
+    cv::Mat& color_volume,                      // global color Volume
+    cv::Mat& model_vertex,                       
+    cv::Mat& model_normal,                        
+    cv::Mat& model_color,
+    const Eigen::Vector3i volume_size,
+    const float voxel_scale,
+    const CameraParameters& cam_parameters,
+    const float truncation_distance,
+    const Eigen::Matrix<float, 3, 3, Eigen::DontAlign> rotation,
+    const Eigen::Matrix<float, 3, 1, Eigen::DontAlign> translation
+)
+{
+    for(int x = 0; x < model_vertex.cols; ++x){
+        for(int y = 0; y < model_vertex.rows; ++y){
+            const Eigen::Vector3f volume_range = volume_size.cast<float>() * voxel_scale; 
+            const Eigen::Matrix<float, 3, 1, Eigen::DontAlign> pixel_position(
+                        (x - cam_parameters.principal_x) / cam_parameters.focal_x,      // X/Z
+                        (y - cam_parameters.principal_y) / cam_parameters.focal_y,      // Y/Z
+                        1.f);
+            Eigen::Matrix<float, 3, 1, Eigen::DontAlign> ray_direction = rotation * pixel_position;
+            ray_direction.normalize();
+
+            float ray_length = fmax(get_min_time(volume_range, translation, ray_direction), 0.f);
+
+            if (ray_length >= get_max_time(volume_range, translation, ray_direction))
+                return;
+            
+            ray_length += voxel_scale;
+            Eigen::Matrix<float, 3, 1, Eigen::DontAlign> grid = (translation + (ray_direction * ray_length)) / voxel_scale;
+
+            float tsdf = static_cast<float>(tsdf_volume.at<cv::Vec<short, 2>>(
+                (static_cast<int>(std::floor(grid(2)) * volume_size[1] + std::floor(grid(1)))),
+                (static_cast<int>(std::floor(grid(0)))))[0]) * DIVSHORTMAX;
+            
+            //TODO: why not sqrt(3) * volume_range[0]?
+            const float max_search_length = ray_length + volume_range[0] * sqrt(2.f);
+
+            for (; ray_length < max_search_length; ray_length += truncation_distance * 0.5f){
+
+                grid = ((translation + (ray_direction * (ray_length + truncation_distance * 0.5f))) / voxel_scale);
+                
+                //checkvalid
+                if (grid.x() < 1 || grid.x() >= volume_size[0] - 1 || 
+                    grid.y() < 1 || grid.y() >= volume_size[1] - 1 ||
+                    grid.z() < 1 || grid.z() >= volume_size[2] - 1 )
+                        continue;
+                
+                const float previous_tsdf = tsdf;
+
+                float tsdf = static_cast<float>(tsdf_volume.at<cv::Vec<short, 2>>(
+                (static_cast<int>(std::floor(grid(2)) * volume_size[1] + std::floor(grid(1)))),
+                (static_cast<int>(std::floor(grid(0)))))[0]) * DIVSHORTMAX;
+
+                // if ray enter from behind
+                if (previous_tsdf < 0.f && tsdf > 0.f)
+                    break;
+
+                //if intersecting zero-crossing
+                if (previous_tsdf > 0.f && tsdf < 0.f){
+
+                    const float t_star =
+                                ray_length - truncation_distance * 0.5f * previous_tsdf / (tsdf - previous_tsdf);
+                    
+                    const auto vertex = translation + ray_direction * t_star;
+
+                    const Eigen::Matrix<float, 3, 1, Eigen::DontAlign> location_in_grid = (vertex / voxel_scale);
+
+                    // check if location is in the grid
+                    if (location_in_grid.x() < 1 || location_in_grid.x() >= volume_size[0] - 1 ||
+                            location_in_grid.y() < 1 || location_in_grid.y() >= volume_size[1] - 1 ||
+                            location_in_grid.z() < 1 || location_in_grid.z() >= volume_size[2] - 1)
+                            break;
+
+                    // caculate normal in this position
+
+                    Eigen::Matrix<float, 3, 1, Eigen::DontAlign> normal;
+                    Eigen::Matrix<float, 3, 1, Eigen::DontAlign> shifted;
+
+                    // x direction
+                    shifted = location_in_grid;
+
+                    shifted.x() += 1.f;
+
+                    if(shifted.x() >= volume_size[0] - 1)
+                        break;
+                    
+                    const float Fx1 = interpolate_trilinearly(
+                        shifted, 
+                        tsdf_volume, 
+                        volume_size, 
+                        voxel_scale);
+
+                    // another shifted direction
+                    shifted = location_in_grid;
+
+                    shifted.x() -= 1.f;
+
+                    if(shifted.x() < 1)
+                        break;
+
+                    const float Fx2 = interpolate_trilinearly(
+                        shifted, 
+                        tsdf_volume, 
+                        volume_size, 
+                        voxel_scale);
+
+                    normal.x() = (Fx1 - Fx2);
+
+                    // y direction
+                    shifted = location_in_grid;
+
+                    shifted.y() += 1;
+
+                    if (shifted.y() >= volume_size[1] - 1)
+                        break;
+
+                    const float Fy1 = interpolate_trilinearly(
+                        shifted, 
+                        tsdf_volume, 
+                        volume_size, 
+                        voxel_scale);
+
+                    shifted = location_in_grid;
+
+                    shifted.y() -= 1;
+
+                    if (shifted.y() < 1)
+                        break;
+
+                    const float Fy2 = interpolate_trilinearly(
+                        shifted, 
+                        tsdf_volume, 
+                        volume_size, 
+                        voxel_scale);
+
+                    normal.y() = (Fy1 - Fy2);
+
+                    // z direction
+                    shifted = location_in_grid;
+
+                    shifted.z() += 1;
+
+                    if (shifted.z() >= volume_size[2] - 1)
+                        break;
+
+                    const float Fz1 = interpolate_trilinearly(
+                        shifted, 
+                        tsdf_volume, 
+                        volume_size, 
+                        voxel_scale);
+
+                    shifted = location_in_grid;
+
+                    shifted.z() -= 1;
+
+                    if (shifted.z() < 1)
+                        break;
+
+                    const float Fz2 = interpolate_trilinearly(
+                        shifted, 
+                        tsdf_volume, 
+                        volume_size, 
+                        voxel_scale);
+
+                    normal.z() = (Fz1 - Fz2);
+
+                    if (normal.norm() == 0)
+                            break;
+
+                    //if normal is not zero, then normalize it
+                    normal.normalize();
+
+                    //save vertex, normal
+                    model_vertex.at<cv::Vec3f>(y,x) = cv::Vec3f(vertex.x(), vertex.y(), vertex.z());
+                    model_normal.at<cv::Vec3f>(y,x) = cv::Vec3f(normal.x(), normal.y(), normal.z());
+
+                    //save color
+                    auto location_in_grid_int = location_in_grid.cast<int>();
+
+                    model_color.at<cv::Vec3b>(y,x) = color_volume.at<cv::Vec3b>((
+                                location_in_grid_int.z() * volume_size[1] +
+                                location_in_grid_int.y()),(location_in_grid_int.x()));
+                    
+                    break;
+            
+                }
+            }
+        }
+    }
+}
+
+void surface_prediction(
+    VolumeData& volume,                   // Global Volume
+    cv::Mat& model_vertex,                       // predicted vertex
+    cv::Mat& model_normal,                       // predicted normal
+    cv::Mat& model_color,                        // predicted color
+    const CameraParameters& cam_parameters,     
+    const float truncation_distance,            
+    const Eigen::Matrix4f& pose)                // camera pose
+{
+    // data praparation, empty the model_vertex, model_normal, model_color
+    model_vertex.setTo(0);
+    model_normal.setTo(0);
+    model_color.setTo(0);
+
+    raycast_tsdf_kernel(
+        volume.tsdf_volume,                 // Global TSDF Volume
+        volume.color_volume,                // Global Color Volume
+        model_vertex,                       // 推理出来的顶点图
+        model_normal,                       // 推理出来的法向图
+        model_color,                        // 推理出来的颜色图
+        volume.volume_size,                 // Volume 尺寸
+        volume.voxel_scale,                 // Volume 缩放洗漱
+        cam_parameters,                     // 当前图层相机内参
+        truncation_distance,                // 截断距离
+        pose.block(0, 0, 3, 3),             // 从相机位姿中提取旋转矩阵
+        pose.block(0, 3, 3, 1));            // 从相机位姿中提取平移向量
+
+}
