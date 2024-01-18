af6257f4747d3547d1e564451733302a75199686
Xuyuan Han <xuyuan.han@outlook.com>
2024-01-19 00:49:51 +0100

    refactor(): change name of surface prediction output image windows

--- src/main.cpp
+++ src/main.cpp
@@ -49,21 +49,21 @@ int main(int argc, char **argv)
         cv::imshow("InputDepth", sensor.getDepth());
         cv::moveWindow("InputDepth", sensor.getColorRGBX().cols, 0);
         
-        cv::imshow("Pipeline Output: Model", pipeline.get_last_model_color_frame());
-        cv::moveWindow("Pipeline Output: Model", 0, sensor.getColorRGBX().rows + 40);
+        cv::imshow("SurfacePrediction Output: Color", pipeline.get_last_model_color_frame());
+        cv::moveWindow("SurfacePrediction Output: Color", 0, sensor.getColorRGBX().rows + 40);
         
-        cv::imshow("Pipeline Output: Vertex", pipeline.get_last_model_vertex_frame());
-        cv::moveWindow("Pipeline Output: Vertex", sensor.getColorRGBX().cols, sensor.getColorRGBX().rows + 40);
+        cv::imshow("SurfacePrediction Output: Vertex", pipeline.get_last_model_vertex_frame());
+        cv::moveWindow("SurfacePrediction Output: Vertex", sensor.getColorRGBX().cols, sensor.getColorRGBX().rows + 40);
 
-        cv::imshow("Pipeline Output: Normal", pipeline.get_last_model_normal_frame());
-        cv::moveWindow("Pipeline Output: Normal", 2 * sensor.getColorRGBX().cols, sensor.getColorRGBX().rows + 40);
+        cv::imshow("SurfacePrediction Output: Normal", pipeline.get_last_model_normal_frame());
+        cv::moveWindow("SurfacePrediction Output: Normal", 2 * sensor.getColorRGBX().cols, sensor.getColorRGBX().rows + 40);
 
         std::string filenameOut = std::string("../output/");
         cv::imwrite(filenameOut + "InputRGB_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", sensor.getColorRGBX());
         cv::imwrite(filenameOut + "InputDepth_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", sensor.getDepth());
-        cv::imwrite(filenameOut + "PipelineOutputColor_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", pipeline.get_last_model_color_frame());
-        cv::imwrite(filenameOut + "PipelineOutputVertex_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", pipeline.get_last_model_vertex_frame());
-        cv::imwrite(filenameOut + "PipelineOutputNormal_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", pipeline.get_last_model_normal_frame());
+        cv::imwrite(filenameOut + "SurfacePredictionOutputColor_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", pipeline.get_last_model_color_frame());
+        cv::imwrite(filenameOut + "SurfacePredictionOutputVertex_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", pipeline.get_last_model_vertex_frame());
+        cv::imwrite(filenameOut + "SurfacePredictionOutputNormal_" + std::to_string(sensor.getCurrentFrameCnt()) + ".png", pipeline.get_last_model_normal_frame());
 
         cv::waitKey(0);
     }
