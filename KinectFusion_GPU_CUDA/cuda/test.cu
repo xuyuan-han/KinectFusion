2bb250f3dd0462bdb37a5c3083cfbc244b45b465
Xuyuan Han <xuyuan.han@outlook.com>
2024-01-21 21:36:58 +0100

    fix(CUDA): fix bug in test code for CUDA environment

--- KinectFusion_GPU_CUDA/cuda/test.cu
+++ KinectFusion_GPU_CUDA/cuda/test.cu
@@ -1,33 +1,32 @@
 #include "kinectfusion.hpp"
 
-__global__ void addKernel(Eigen::MatrixXf *a, Eigen::MatrixXf *b, Eigen::MatrixXf *c, int N) {
+__global__ void addKernel(float *a, float *b, float *c, int N) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < N) {
-        (*c)(i) = (*a)(i) + (*b)(i);
+        c[i] = a[i] + b[i];
     }
 }
 
 int test_cuda() {
     int N = 10;
 
-    // Allocate host memory
-    Eigen::MatrixXf h_a(N, 1), h_b(N, 1), h_c(N, 1);
+    // Allocate host memory and initialize matrices
+    Eigen::VectorXf vec_a = Eigen::VectorXf::LinSpaced(N, 0, N-1);
+    Eigen::VectorXf vec_b = 2 * vec_a;
 
-    // Initialize matrices
-    for (int i = 0; i < N; i++) {
-        h_a(i) = static_cast<float>(i);
-        h_b(i) = static_cast<float>(i * 2);
-    }
+    Eigen::MatrixXf h_a = vec_a;
+    Eigen::MatrixXf h_b = vec_b;
+    Eigen::MatrixXf h_c(N, 1);
 
     // Allocate device memory
-    Eigen::MatrixXf *d_a, *d_b, *d_c;
-    cudaMalloc((void **)&d_a, sizeof(Eigen::MatrixXf));
-    cudaMalloc((void **)&d_b, sizeof(Eigen::MatrixXf));
-    cudaMalloc((void **)&d_c, sizeof(Eigen::MatrixXf));
+    float *d_a, *d_b, *d_c;
+    cudaMalloc((void **)&d_a, N * sizeof(float));
+    cudaMalloc((void **)&d_b, N * sizeof(float));
+    cudaMalloc((void **)&d_c, N * sizeof(float));
 
     // Copy data from host to device
-    cudaMemcpy(d_a, &h_a, sizeof(Eigen::MatrixXf), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, &h_b, sizeof(Eigen::MatrixXf), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, h_a.data(), N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b.data(), N * sizeof(float), cudaMemcpyHostToDevice);
 
     // Launch kernel
     int blockSize = 4;
@@ -35,15 +34,12 @@ int test_cuda() {
     addKernel<<<numBlocks, blockSize>>>(d_a, d_b, d_c, N);
 
     // Copy data back to host
-    cudaMemcpy(&h_c, d_c, sizeof(Eigen::MatrixXf), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_c.data(), d_c, N * sizeof(float), cudaMemcpyDeviceToHost);
 
     // Print results
-    std::cout << "Matrix A:" << std::endl;
-    std::cout << h_a << std::endl;
-    std::cout << "Matrix B:" << std::endl;
-    std::cout << h_b << std::endl;
-    std::cout << "Result:" << std::endl;
-    std::cout << h_c << std::endl;
+    std::cout << "Matrix A:" << std::endl << h_a << std::endl;
+    std::cout << "Matrix B:" << std::endl << h_b << std::endl;
+    std::cout << "Result:" << std::endl << h_c << std::endl;
 
     // Free device memory
     cudaFree(d_a);
