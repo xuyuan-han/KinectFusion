93cb8ebbf702b96d5f166e3af571e419be707876
Xuyuan Han <xuyuan.han@outlook.com>
2024-01-21 20:24:31 +0100

    feat(CUDA): update CUDA header file include

--- KinectFusion_GPU_CUDA/include/kinectfusion.hpp
+++ KinectFusion_GPU_CUDA/include/kinectfusion.hpp
@@ -1,7 +1,6 @@
-#include <cuda_runtime.h>
-#include <cuda_profiler_api.h>
-#include <device_launch_parameters.h>
-
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
 #include <Eigen/Dense>
 #include <iostream>
